577d51be54999b639dea1d331ae2543838a39f55
Sigit Wicaksono <Sigit Wicaksono@SigitWicaksono>
2017-10-31 21:00:34 +0700

    BinaryTree

--- binary_and_other_trees/src/binary_and_other_trees/main.java
+++ binary_and_other_trees/src/binary_and_other_trees/main.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package binary_and_other_trees;
-
-/**
- *
- * @author Sigit Wicaksono
- */
 public class main {
     public static void main(String []args){
         BinaryTreeNode tree = new BinaryTreeNode();
@@ -18,16 +8,10 @@ public class main {
         tree.root.leftChild.leftChild = new BinaryTreeNode(4);
         tree.root.leftChild.rightChild = new BinaryTreeNode(5);
  
-//        System.out.println("Preorder traversal of binary tree is ");
-//        tree.preOrder();
-        BinaryTreeNode tre = (BinaryTreeNode) tree.Clone(tree);
-        tre.preOrder();
-        
-// 
-//        System.out.println("\nInorder traversal of binary tree is ");
-//        tree.printInorder();
-// 
-//        System.out.println("\nPostorder traversal of binary tree is ");
-//        tree.printPostorder();
+        System.out.println("Preorder traversal of binary tree is ");
+        tree.preOrder();
+  
+        System.out.println("\nPostorder traversal of binary tree is ");
+        tree.postOrder();
     }
 }
